ef1634e8a10e035accf0fb94c07de95cec52a06b
Mohamad AbdelMon’em <mabdelmonem.c@saip.gov.sa>
2024-10-29 12:42:09 +0300

    init mantine ui and react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createTheme, MantineProvider } from '@mantine/core';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import '@mantine/core/styles.css';
+import router from './routes.tsx';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </StrictMode>
+);
